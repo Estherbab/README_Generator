1c6605cc344324177d7a3a9592c9fb723d82d897
Estherbab <153003348+Estherbab@users.noreply.github.com>
2024-02-19 19:49:37 +0000

    Added code to fs.write file funtion for filename & data

--- starter/index.js
+++ starter/index.js
@@ -10,42 +10,68 @@ const questions = [
         name: 'Title',
         message: 'What is your project Title?',
       },
+
       {
         type: 'input',
         name: 'Description',
         message: 'Describe what your project is about?',
       },
+
       {
         type: 'input',
-        name: 'hobby',
-        message: 'What is your favorite hobby?',
+        name: 'Installation',
+        message: 'What are the intallation instructions?',
       },
+
       {
         type: 'input',
-        name: 'food',
-        message: 'What is your favorite food?',
+        name: 'Usage',
+        message: 'What does the app do?',
       },
+
       {
         type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username',
+        name: 'License',
+        message: 'What license has been used on this Repo?',
       },
+
       {
         type: 'input',
-        name: 'linkedin',
-        message: 'Enter your LinkedIn URL.',
+        name: 'Contributing',
+        message: 'How do you contribute to this Repo?',
+      },
+
+      {
+        type: 'input',
+        name: 'Tests',
+        message: 'What are the instructions on testing?',
+      },
+
+      {
+        type: 'input',
+        name: 'Questions_Github',
+        message: 'What is your github user name?',
+      },
+
+      {
+        type: 'input',
+        name: 'Questions_Email',
+        message: 'What is your email address',
       },
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
+// function to write README file with 2 parameters file name and data
+function writeToFile(fileName, data) {        
+    fs.writeFile(fileName, data, (err) =>
+  err ? console.error(err) : console.log('Success!')
+);
 }
 
 // function to initialize program
 function init() {
 inquirer.prompt(questions)
 .then(answers =>{
-    console.log(answers)
+    console.log(answers)   // getting our answers from all of our questions 
 })
 }
 
