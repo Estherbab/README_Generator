597e389a6512246d3910ade5bafa9d7cbbd794be
Estherbab <153003348+Estherbab@users.noreply.github.com>
2024-02-19 19:24:17 +0000

    Added array questions & function for Q&As

--- starter/index.js
+++ starter/index.js
@@ -5,7 +5,36 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+      },
+      {
+        type: 'input',
+        name: 'location',
+        message: 'Where are you from?',
+      },
+      {
+        type: 'input',
+        name: 'hobby',
+        message: 'What is your favorite hobby?',
+      },
+      {
+        type: 'input',
+        name: 'food',
+        message: 'What is your favorite food?',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+      },
+      {
+        type: 'input',
+        name: 'linkedin',
+        message: 'Enter your LinkedIn URL.',
+      },
 ];
 
 // function to write README file
@@ -14,7 +43,10 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+inquirer.prompt(questions)
+.then(answers =>{
+    console.log(answers)
+})
 }
 
 // function call to initialize program
